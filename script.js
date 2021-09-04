33ac1ba06ed723b8827fee3a4f430922e58c5ab0
kk-op2 <85089825+kk-op2@users.noreply.github.com>
2021-09-04 22:39:14 +0530

    first commit

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 'use strict';
+alert(
+  'Rules \n 1.First Player to score 100 points win the game 2.Rolling Dice 1 will shift your turn.'
+);
 
 // Selecting Elements
 const player0EL = document.querySelector('.player--0');
